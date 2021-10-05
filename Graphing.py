f267b9e726f1e5f80597904a97dba51ac5a657a2
Squidy-Boi <57235118+Squidy-Boi@users.noreply.github.com>
2021-10-05 11:57:55 -0400

    Add files via upload

--- Graphing.py
+++ Graphing.py
@@ -22,22 +22,36 @@ from sympy import *
 import random
 
 
-# In[10]:
+# In[ ]:
 
 
+def graph(formula, x_range):  
+    x = np.array(x_range)  
+    y = eval(formula)
+    plt.plot(x, y)
+    plt.show()
+
+def graph2(formula, x_range, color, lowerBound, upperBound):
+    x = np.array(x_range)
+    y = eval(formula)
+    plt.ylim(lowerBound,upperBound)
+    plt.plot(x,y)
+    
 while True:
     while True:
         try:
-            answer = int(input("graph equation, plot points or solve an equation? (1,2,3): "))
+            answer = int(input("graph equation, plot points, solve an equation, or graph multiple? (1,2,3,4): "))
             if answer == 1:
                 break
             elif answer == 2:
                 break
             elif answer == 3:
                 break
+            elif answer == 4:
+                break
         except ValueError:
             answer = 0
-            print("it says 1 or 2 cmon man")
+            print("invalid syntax: 1,2,3 or 4")
     if answer == 2:
         x = str
         y = str
@@ -52,35 +66,54 @@ while True:
             d.append(int(input("Enter Y: ")))
         fig = px.scatter(x=b,y=d,trendline='ols')
         fig.show()
-        break
     elif answer == 1: 
         leftBound = int(input("Enter LeftBound: "))
         rightBound = int(input("Enter RightBound: "))
         userEq = input("Enter Equation: y = ")
-
-        def graph(formula, x_range):  
-            x = np.array(x_range)  
-            y = eval(formula)
-            plt.plot(x, y)
-            plt.show()
         
         graph(userEq, range(leftBound, rightBound))
     elif answer == 3:
+        while True:  
+            userEq = input("Enter Equation y = ")
+            xval = input("Enter x value: ")
+            userEq = userEq.replace("x", xval)
+            try: 
+                y = eval(userEq)
+                print("The answer is: " + str(y))
+                break
+            except ZeroDivisionError:
+                print("Undefined")
+                break
+            except SyntaxError:
+                print("invalid syntax retype expression")
+                y 
+            except NameError:
+                print("invalid syntax retype expression")
+    elif answer == 4:
+        color = ["r","b","g","c","m","y","w","k"]
+        amt = 0
         
-        userEq = input("Enter Equation y = ")
-        xval = input("Enter x value: ")
-        userEq = userEq.replace("x", xval)
-        try: 
-            y = eval(userEq)
-            print("The answer is: " + str(y))
-            break
-        except ZeroDivisionError:
-            print("Undefined")
-            break
-        
+        amt = int(input("How many graphs? (max of 8): "))
+        if amt > 8:
+            while amt > 8:
+                amt = int(input("How many graphs? (max of 8): "))
+        leftBound = int(input("Enter LeftBound: "))
+        rightBound = int(input("Enter RightBound: "))
+        upperBound = int(input("Enter UpperBound: "))
+        lowerBound = int(input("Enter LowerBound: "))
+        for i in range(amt):
+            tempColor = color[0]
+            userEq = input("Enter Equation: y =")
+            graph2(userEq, range(leftBound,rightBound),tempColor,lowerBound,upperBound)
+            color.pop(0)
+        plt.show()
 
 
-# # 
+# In[ ]:
+
+
+
+
 
 # In[ ]:
 
